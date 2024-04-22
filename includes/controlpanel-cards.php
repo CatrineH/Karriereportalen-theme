a4397ed28346c2d96b8cbe6c98df012236aa0811
Catrine <70755662+CatrineH@users.noreply.github.com>
2024-04-22 12:21:49 +0200

    update C-panel cards

--- includes/controlpanel-cards.php
+++ includes/controlpanel-cards.php
@@ -7,7 +7,7 @@
     </div>
             <div class="card-body text-center">
                     <h5 class="card-title">Opprett annonse</h5>
-                    <p class="card-text">Kom i gang nå, start med å utlyse stillingen din, husk å få med alle detaljer for søkerne.</p>
+                    <p  styles="font-size: 18px;" class="card-text">Kom i gang nå, start med å utlyse stillingen din, husk å få med alle detaljer for søkerne.</p>
              </div>
     </div>
        
@@ -18,7 +18,7 @@
     </div>
             <div class="card-body text-center">
                     <h5 class="card-title">Se statistikk</h5>
-                    <p class="card-text">Vi har gjort det enkelt for deg å se trafikken på annonseringene dine.</p>
+                    <p styles="font-size: 18px;"class="card-text">Vi har gjort det enkelt for deg å se trafikken på annonseringene dine.</p>
             </div>
     </div>
         
@@ -28,7 +28,7 @@
         </div>
             <div class="card-body text-center">
                     <h5 class="card-title">FAQ</h5>
-                    <p class="card-text">Sitter du fast? Sjekk våre ofte stilte spørsmål for hjelp.</p>
+                    <p styles="font-size: 18px;" class="card-text">Sitter du fast? Sjekk våre ofte stilte spørsmål for hjelp.</p>
             </div>
             
         
