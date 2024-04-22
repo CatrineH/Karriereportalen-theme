55751a87488e68f99ba3fa815517ddf43a2f2b80
Catrine <70755662+CatrineH@users.noreply.github.com>
2024-04-22 12:03:19 +0200

    update file

--- includes/login-form.php
+++ includes/login-form.php
@@ -1,26 +1,30 @@
-
+<!-- Login Form -->
 <form class="form" action="<?php echo wp_login_url(); ?>" method="post">
-      <h2>Logg inn</h2>
-      <div class="form-field">
-        <label for="email">Brukernavn</label>
-        <input type="email" id="email" placeholder="Din e-post">
-      </div>
-      <div class="form-field">
-        <label for="password">Passord</label>
-        <input type="password" id="password" placeholder="Ditt passord">
-        <a href="#" class="forgot-password">Glemt passord?</a>
-      </div>
-      
-      <div class="form-action">
-        <label class="checkbox-container">
-          Husk meg
-          <input type="checkbox" id="remember-me">
-          <span class="checkmark"></span>
-        </label>
-        <button type="submit" class="login-button">Logg inn</button>
-      </div>
-      <a href="register" class="register">Opprett konto</a>
-    </form>
+  <h2>Logg inn</h2>
+  
+  <div class="form-field">
+    <label for="user_login">Brukernavn</label><br>
+    <input type="text" name="log" id="user_login" placeholder="Din e-post" required>
+  </div>
+
+  <div class="form-field">
+    <label for="user_pass">Passord</label><br>
+    <input type="password" name="pwd" id="user_pass" placeholder="Ditt passord" required>
+  </div>
+
+  <div class="form-action">
+    <label class="checkbox-container"> Husk meg
+      <input type="checkbox" name="rememberme" id="remember-me" value="forever">
+      <span class="checkmark"></span>
+    </label>
+    <a href="<?php echo wp_lostpassword_url(); ?>" class="forgot-password">Glemt passord</a>
+  </div>
+
+  <button type="submit" name="wp-submit" class="login-button">Logg inn</button>
+<br>
+<p>Har du ikke en bruker hos oss?
+  <a href="<?php echo get_permalink(get_page_by_path('register')); ?>" class="register">Opprett en konto</a>
+</p>
 
 
-
+</form>
