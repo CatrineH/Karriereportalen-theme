1b33888911e70fa6a2ee5dfa900ac33f411d9b11
Catrine <70755662+CatrineH@users.noreply.github.com>
2024-04-22 11:59:38 +0200

    update file

--- footer.php
+++ footer.php
@@ -1,36 +1,40 @@
-
 <?php
 /**
  * Footer Template
- *  @package Karriereportalen
+ * @package Karriereportalen
  */
-
 ?>
 
-
-<?php wp_footer();?>
-<!-- <div class="container"> -->
-<footer class="footer fixed-bottom"> 
-        <div class="footer-column">
-            <a href="<?php echo home_url();  ?>"class="logo">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Kristiania_logo_22_hvit.png" alt="Footer Logo" />
-            </a>
-            <span class="footer-separator">|</span>
-            <div class="contact-info">Kontakt oss<br><br>
-                <a href="#">karriere@kristiania.no</a><br>
-                <a href="#">+47 99 15 07 42</a><br><br>
-                <p>Karriereportalen driftes av karrieresenteret.</p>
-            </div>
+    <footer class="container-fluid py-3 text-left text-white bg-black p-5">
+      <div class="row">
+        <div class="col-12 col-md-4 mb-3 mb-md-0">
+          <a href="<?php echo home_url(); ?>" class="navbar-brand">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Kristiania_logo_22_hvit.png" alt="Kristiania" style="height: 70px;"> <!-- Adjust logo size as needed -->
+          </a>
         </div>
-        <div class="social-media-icons d-inline p-4 pe-auto">
-    <a href="#" aria-label="Facebook"><i class="fa-brands fa-facebook"></i></a>
-    <a href="#" aria-label="Twitter"><i class="fa-brands fa-twitter"></i></a>
-    <a href="#" aria-label="Instagram"><i class="fa-brands fa-instagram"></i></a>
-    <a href="#" aria-label="Facebook"> <i class="fa-brands fa-linkedin"></i></a>
-</div>
+        <div class="col-12 col-md-4 mb-4 mb-md-0 ">
+          <h5> Kontakt oss</h5><br>
+          karriere@kristiania.no<br>
+          +47 99 15 07 42
+        </div>
+        <div class="col-12 col-md-4">
+          <!-- Social Media Icons -->
+          <div class="social-icons">
+            <a href="#" aria-label="Facebook"><i class="fab fa-facebook-f"></i></a>
+            <a href="#" aria-label="Twitter"><i class="fab fa-twitter"></i></a>
+            <a href="#" aria-label="Instagram"><i class="fab fa-instagram"></i></a>
+            <a href="#" aria-label="LinkedIn"><i class="fab fa-linkedin-in"></i></a>
+          </div>
+        </div>
+      </div>
+    </footer>
 
-</div>
-</footer>
-
-</body>
+    <?php wp_footer(); ?>
+  </body>
 </html>
+
+
+
+
+
+
