3b7565305f0ea7858ff1be3a2ea45ad2bd066075
Catrine <70755662+CatrineH@users.noreply.github.com>
2024-04-23 17:52:59 +0200

    update

--- includes/controlpanel-sidebar.php
+++ includes/controlpanel-sidebar.php
@@ -1,7 +1,5 @@
-
- <!-- Sidebar -->
- <aside class="col-md-2 d-none d-md-block sidebar">
-        <div class="sidebar-sticky">
+<aside class="col-md-2 d-none d-md-block sidebar">
+    <div class="sidebar-sticky">
  
         <div class="card" style="width: 15rem; height:35rem; border-radius: 8px; align-items: center; ">
            
@@ -12,27 +10,29 @@
                 </div>   
             </div>
 
-            <ul class="nav flex-column">
-  <li class="nav-item">
-  <a href="kontrollpanel"><i class="fa fa-dashboard"></i> Kontrollpanel</a>
-  </li>
-  <li class="nav-item">
-  <a href="annonsering"><i class="fa fa-plus-square"></i> Opprett ny annonse</a>
-  </li>
-  <li class="nav-item">
-  <a href="faq"><i class="fa fa-life-ring"></i> Support</a>
-  </li>
-  <li class="nav-item">
-  <a href="#"><i class="fa fa-cog"></i> Innstillinger</a>
-  </li>
-  <li class="nav-item">
-  <a href="#"><i class="fa fa-sign-out"></i> Logg ut</a>
-  </li>
-</ul>
-        </div>
+    <ul class="nav flex-column">
 
+        <li class="nav-item">
+            <a href="kontrollpanel"><i class="fa fa-dashboard"></i> Kontrollpanel</a>
+        </li>
 
+         <li class="nav-item">
+             <a href="annonsering"><i class="fa fa-plus-square"></i> Opprett ny annonse</a>
+         </li>
 
-      
-        </div>
-      </aside>
+         <li class="nav-item">
+            <a href="faq"><i class="fa fa-life-ring"></i> Support</a>
+         </li>
+
+         <li class="nav-item">
+             <a href="#"><i class="fa fa-cog"></i> Innstillinger</a>
+        </li>
+
+        <li class="nav-item">
+            <a href="#"><i class="fa fa-sign-out"></i> Logg ut</a>
+         </li>
+         
+    </ul>
+</div>      
+ </div>
+</aside>
