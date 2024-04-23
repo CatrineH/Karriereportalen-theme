942cdcc6ad5bc7bc6a28530a7c944e214288eb59
Catrine <70755662+CatrineH@users.noreply.github.com>
2024-04-23 16:00:16 +0200

    update c-panel

--- includes/controlpanel-sidebar.php
+++ includes/controlpanel-sidebar.php
@@ -2,7 +2,14 @@
  <!-- Sidebar -->
  <aside class="col-md-2 d-none d-md-block sidebar">
         <div class="sidebar-sticky">
-         
+ 
+            <div class="text-left ">
+                <h3 class="mt-4" style="color: #9E182F;">Hei Marianne hos Amediaaa!</h3>
+                <h6 style="color: black;">Her er ditt kontrollpanel.</h6>
+            </div>   
+            <br> <br>   
+
+
 <ul class="nav flex-column">
   <li class="nav-item">
   <a href="kontrollpanel"><i class="fa fa-dashboard"></i> Kontrollpanel</a>
--- page-templates/controlpanel-page.php
+++ page-templates/controlpanel-page.php
@@ -3,25 +3,21 @@
 
 get_header();
 ?>
-<div id="wrapper" class="d-flex">
+
     <!-- Sidebar -->
-    <div class="bg-red p-4" id="sidebar-wrapper">
+    <div class="sidebar" id="sidebar-wrapper">
     <?php get_template_part('includes/controlpanel-sidebar'); ?>
     </div>
     <!-- Page Content -->
     <div id="page-content-wrapper" >
         <div class="container">
-            <div class="text-left ">
-                <h2 class="mt-4" style="color: black;">Hei Marianne hos Amediaaa!</h2>
-                <h5 style="color: #9E182F;">Velkommen til karriereportalen.</h5>
-            </div>
-            
-            <!-- Cards Section -->
+        <div class="d-flex justify-content-center">
+
             <div class="card-container d-flex justify-content-center flex-wrap">
               <?php get_template_part('includes/controlpanel-cards'); ?>  
             </div>
 
         </div>
     </div>
-</div>
+<!-- </div> -->
 <?php get_footer(); ?>
