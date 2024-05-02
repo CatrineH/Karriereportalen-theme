b1317696d2901a02a0df74c3441fc0b16b337cb6
atmil <128690694+atmil@users.noreply.github.com>
2024-05-02 19:08:31 +0200

    Oppdaterte FAQ

--- includes/faq-template.php
+++ includes/faq-template.php
@@ -38,12 +38,12 @@
   <div class="accordion-item">
     <h2 class="accordion-header">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-      FAQ 3
+      Hva er et banner?
       </button>
     </h2>
     <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordion">
       <div class="accordion-body">
-      <strong>This is the third item's accordion body.</strong> Put content in here.
+      <strong>Et banner er vanligvis et stort bilde som vises øverst på en nettside.</strong> Det kan for eksempel være et bilde av bedriften, kampanje eller et slagord. 
       </div>
     </div>
   </div>
@@ -52,12 +52,12 @@
 <div class="accordion-item">
     <h2 class="accordion-header">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-      FAQ 4
+      Hva er forskjellen på en annonse og en bedriftside?
       </button>
     </h2>
     <div id="collapseFour" class="accordion-collapse collapse" data-bs-parent="#accordion">
       <div class="accordion-body">
-      <strong>This is the fourth item's accordion body.</strong> Put content in here.
+      <strong></strong> En annonse inneholder informasjon om stillingen som utlyses, som for eksempel stillingstittel, arbeidsoppgaver og arbeidssted. Mens en bedriftside er en dedikert side til å presentere informasjon om din bedrift. Den inneholder vanligvis detaljer om hva bedriften gjør, kontaktinformasjon og historie.
       </div>
     </div>
   </div>
@@ -67,12 +67,12 @@
 <div class="accordion-item">
     <h2 class="accordion-header">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
-      FAQ 5
+      Hvordan lager jeg en profil? 
       </button>
     </h2>
     <div id="collapseFive" class="accordion-collapse collapse" data-bs-parent="#accordion">
       <div class="accordion-body">
-      <strong>This is the fifth item's accordion body.</strong> Put content in here.
+      <strong>Du kan enkelt lage en profil ved å trykke "Opprett en konto" på hovedsiden.</strong> Deretter følger du stegene og fyller inn nødvendig informasjon. 
       </div>
     </div>
   </div>
