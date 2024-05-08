bf1dfe322453bd22ae6f4c3022693d614ca2a645
Catrine <70755662+CatrineH@users.noreply.github.com>
2024-05-08 23:41:04 +0200

    Updated some text changes to FAQ - Still having issue with layout

--- includes/faq-template.php
+++ includes/faq-template.php
@@ -1,40 +1,29 @@
-<!-- <body <?php body_class('faq-page'); ?> -->
-
-<div class="container-fluid mb-3" >
-  <h3 class="mb-4">Ofte stilte spørsmål</h3>
-  
-  <div class="accordion" id="accordion">
-
+<h3 class="mb-4">Ofte stilte spørsmål</h3>
+    <div class="accordion" id="accordion">
   <div class="accordion-item">
     <h2 class="accordion-header">
       <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        Hvor finner jeg verifiseringskoden? 
+      Hvor finner jeg verifiseringskoden? 
       </button>
     </h2>
     <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordion">
-      <div class="accordion-body" style="white-space: nowrap; >
+      <div class="accordion-body">
         <strong>Verifiseringskoden er blitt sendt på mail. </strong> Du bør sjekke søppelposten dersom du ikke finner den i innboksen.
       </div>
     </div>
   </div>
-
-
-
   <div class="accordion-item">
     <h2 class="accordion-header">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-      Hvordan laster jeg opp logo?
+      Hvordan laster jeg opp en logo?
       </button>
     </h2>
     <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordion">
-      <div class="accordion-body" style="white-space: nowrap;>
-      <strong>Bildet må være innenfor den gitte størrelsen.</strong> Bildet må være 60x60.
+      <div class="accordion-body">
+      <strong>Logoen må være innenfor den gitte størrelsen.</strong> Bildet må være 60x60px (pixler).
       </div>
     </div>
   </div>
-
-
-
   <div class="accordion-item">
     <h2 class="accordion-header">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
@@ -42,38 +31,36 @@
       </button>
     </h2>
     <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordion">
-      <div class="accordion-body" style="white-space: nowrap;>
+      <div class="accordion-body">
       <strong>Et banner er vanligvis et stort bilde som vises øverst på en nettside.</strong> Det kan for eksempel være et bilde av bedriften, kampanje eller et slagord. 
       </div>
     </div>
   </div>
-
-
-<div class="accordion-item">
+  <div class="accordion-item">
     <h2 class="accordion-header">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
       Hva er forskjellen på en annonse og en bedriftside?
       </button>
     </h2>
     <div id="collapseFour" class="accordion-collapse collapse" data-bs-parent="#accordion">
-      <div class="accordion-body" style="white-space: nowrap;>
-      <strong></strong> En annonse inneholder informasjon om stillingen som utlyses, som for eksempel stillingstittel, arbeidsoppgaver og arbeidssted. Mens en bedriftside er en dedikert side til å presentere informasjon om din bedrift. Den inneholder vanligvis detaljer om hva bedriften gjør, kontaktinformasjon og historie.
+      <div class="accordion-body">
+      <strong>En annonse inneholder informasjon om stillingen som utlyses, som for eksempel stillingstittel, arbeidsoppgaver og arbeidssted.</strong>
+        <br>  Mens en bedriftside er en dedikert side til å presentere informasjon om din bedrift. Den inneholder vanligvis detaljer om hva bedriften gjør, kontaktinformasjon og historie.
       </div>
     </div>
   </div>
-
-
-
-<div class="accordion-item">
+  <div class="accordion-item">
     <h2 class="accordion-header">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
       Hvordan lager jeg en profil? 
       </button>
     </h2>
     <div id="collapseFive" class="accordion-collapse collapse" data-bs-parent="#accordion">
-      <div class="accordion-body" style="white-space: nowrap;>
+      <div class="accordion-body">
       <strong>Du kan enkelt lage en profil ved å trykke "Opprett en konto" på hovedsiden.</strong> Deretter følger du stegene og fyller inn nødvendig informasjon. 
       </div>
     </div>
   </div>
+
+
 </div>
--- page-templates/faq-page.php
+++ page-templates/faq-page.php
@@ -5,18 +5,14 @@ get_header();
 
 <?php get_template_part('includes/controlpanel-sidebar'); ?> 
 
-<div id="page-content-wrapper">
     <div class="container">
       <div class="row">
         <?php get_template_part('includes/faq-template'); ?>         
           </div>
           </div>
-</div>
 
-    <!-- Sidebar -->
-    <div class="sidebar" id="sidebar-wrapper">
-    <?php get_template_part('includes/controlpanel-sidebar'); ?>
-    </div>
+
+
 <?php
 get_footer();
 ?>
