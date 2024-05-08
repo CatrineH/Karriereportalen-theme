043df24db6d995ae663fe6244a9f237b36642886
Catrine <70755662+CatrineH@users.noreply.github.com>
2024-05-08 21:32:55 +0200

    Frontend updated regarding design in figma

--- includes/job-posting-form.php
+++ includes/job-posting-form.php
@@ -1,204 +1,259 @@
-<form id="job-listing-form" style="background: #9E182F; padding: 30px;" action="#husk_prosess_script" method="post" enctype="multipart/form-data">
-    <div class="container-fluid">
-        <div class="card custom-card-2">
+
+
+
+
+
+ <!-- Form -->
+ <form id="job-listing-form" style="" action="#husk_prosess_script" method="post" enctype="multipart/form-data">
+        <div class="card" style="">
             <div class="card-header">
-                <h6 style="color: grey;">Annonserings detaljer - fyll ut skjema*</h6>
+                <h6 style="color: grey;"> Stillingsannonse utkast</h6>
             </div>
-            <div class="card-body">
-                <fieldset>
+
+<div class="card-body col-lg-12">
+  <fieldset>
 
 <!-- Form Name -->
-<legend>Stillingsannonse</legend>
+<legend></legend>
 
-<!-- File Button --> 
-<div class="form-group">
-  <label class="col-md-4 control-label" for="logo">Logo</label>
-  <div class="col-md-8">
-    <input id="logo" name="logo" class="input-file" type="file">
-  </div>
-</div>
 
-<!-- File Button --> 
+ <!-- --------------------------------- BANNER ------------------------------------>
 <div class="form-group">
-  <label class="col-md-4 control-label" for="Banner">Annonsebanner</label>
-  <div class="col-md-8">
-    <input id="Banner" name="Banner" class="input-file" type="file">
-  </div>
-</div>
-<hr>
-<!-- Text input-->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="textinput">Stillingstittel</label>  
-  <div class="col-md-8">
-  <input id="textinput" name="textinput" type="text" placeholder="" class="form-control input-md" required="">
-    
-  </div>
-</div>
+<!-- <img id="bannerPreview" src="https://via.placeholder.com/950x200" alt="Banner Preview"/> -->
+<img id="bannerPreview" src="https://via.placeholder.com/1000x200" alt="Banner Preview"/>
 
-<!-- Select Basic -->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="sektor">Velg sektor</label>
-  <div class="col-md-8">
-    <select id="sektor" name="sektor" class="form-control">
-      <option value="1">-</option>
-      <option value="2">Privat</option>
-      <option value="">Offentlig</option>
-    </select>
-  </div>
-</div>
+  <label class="col-lg-4 control-label" for="Banner"></label>
+  <div class="col-lg-12">
 
-<!-- Select Basic -->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="selectbransje">Velg bransje</label>
-  <div class="col-md-8">
-    <select id="selectbransje" name="selectbransje" class="form-control">
-      <option value="1">-</option>
-      <option value="2">Helse og omsorg</option>
-      <option value="3">Varehandel</option>
-      <option value="4">Industri</option>
-      <option value="5">Bygg og anlegg</option>
-      <option value="6">Undervisning</option>
-      <option value="7">Offentlig administrasjon</option>
-      <option value="8">Faglige tjenester</option>
-      <option value="9">IT og medier</option>
-      <option value="10">Olje og gass</option>
-      <option value="">Finans og forsikring</option>
-    </select>
+  <input id="banner" name="banner" class="input-file" type="file" accept="image/*" onchange="previewImage('banner', 'bannerPreview')">
+
+  <!-- <input id="banner" name="banner" class="input-file" type="file" accept="image/*" onchange="previewImage('banner', 'bannerPreview')"> -->
   </div>
 </div>
 
 
+ <!-- ------------------------------------ LOGO --------------------------------->
+ <div class="form-group" style="margin-top: 30px;">
 
+<img id="logoPreview" src="https://via.placeholder.com/150x150" alt="Logo Preview"/>  
 
-<!-- Text input-->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="Arbeidsgiver">Arbeidsgiver</label>  
-  <div class="col-md-8">
-  <input id="Arbeidsgiver" name="Arbeidsgiver" type="text" placeholder="" class="form-control input-md" required="">
-    
-  </div>
+  <label class="col-lg-4 control-label" for="logo"></label>
+  <div class="col-lg-8">
+  <input id="logo" name="logo" class="input-file" type="file" accept="image/*" onchange="previewImage('logo', 'logoPreview')">
 </div>
-
-<!-- Text input-->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="sted">Arbeidsted</label>  
-  <div class="col-md-8">
-  <input id="sted" name="sted" type="text" placeholder="" class="form-control input-md" required="">
-    
-  </div>
 </div>
+<!-- ---------------------------------------------------------------------------->
 
-<!-- Text input-->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="antstillinger">Antall stillinger</label>  
-  <div class="col-md-8">
-  <input id="antstillinger" name="antstillinger" type="text" placeholder="1 >" class="form-control input-md" required="">
-    
+ <!-- --- ANNONSETITTEL ----  -->
+ <div class="row">
+
+<div class="form-group" style="margin-top: 30px;">
+  <label class="control-label" for="textinput">Annonsetittel</label>  
+
+  <input id="textinput" name="textinput" type="text" placeholder="" class="form-control input-lg" required="">
   </div>
-</div>
+  </div>
+  <div class="w-100">
 
-<!-- Multiple Radios -->
-<div class="form-group">
-    <label class="col-md-4 control-label" for="ansettelsesform">Ansettelsesform</label>
-    <div class="col-md-8">
-        <div class="row">
-           
-            <div class="col-md-6">
-                <div class="form-check">
-                    <input class="form-check-input" type="radio" name="ansettelsesform" id="ansettelsesform-0" value="1" checked>
-                    <label class="form-check-label" for="ansettelsesform-0">Fulltid</label>
-                </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="radio" name="ansettelsesform" id="ansettelsesform-1" value="2">
-                    <label class="form-check-label" for="ansettelsesform-1">Deltid</label>
-                </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="radio" name="ansettelsesform" id="ansettelsesform-2" value="3">
-                    <label class="form-check-label" for="ansettelsesform-2">Vikariat</label>
-                </div>
-            </div>
-            
-            <div class="col-md-6">
-                <div class="form-check">
-                    <input class="form-check-input" type="radio" name="ansettelsesform" id="ansettelsesform-3" value="4">
-                    <label class="form-check-label" for="ansettelsesform-3">Intern</label>
-                </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="radio" name="ansettelsesform" id="ansettelsesform-4" value="5">
-                    <label class="form-check-label" for="ansettelsesform-4">Frivillig</label>
-                </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="radio" name="ansettelsesform" id="ansettelsesform-5" value="6">
-                    <label class="form-check-label" for="ansettelsesform-5">Annet</label>
-                </div>
-            </div>
-        </div>
+ <!----------------------------------------------- ROW 1 -------- ------------------------ -->
+
+  <!------ STILLINGSTITTEL ---- -->
+ <div class="row">
+  <div class="col-lg-6">
+    <div class="form-group" style="margin-top: 30px;">
+      <label class="control-label" for="textinput">Stillingstittel</label>  
+      <input id="textinput" name="textinput" type="text" placeholder="" class="form-control input-lg" required="">
     </div>
-</div>
-
-
-<!-- Textarea -->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="beskrivelse">Stillingsbeskrivelse</label>
-  <div class="col-md-8">                     
-    <textarea class="form-control" id="beskrivelse" name="beskrivelse">Informasjon om stillingen</textarea>
   </div>
-</div>
-
-<!-- Text input-->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="dato">Frist for søknad</label>  
-  <div class="col-md-8">
-  <input id="dato" name="dato" type="text" placeholder="01.01.01" class="form-control input-md" required="">
-    
-  </div>
-</div>
-<hr>
-<!-- File Button --> 
-<div class="form-group">
-  <label class="col-md-4 control-label" for="lenke">Lenke til ekstern søkeportal</label>
-  <div class="col-md-8">
-    <input id="lenke" name="lenke" class="input-file" type="file">
-  </div>
-</div>
-
-<!-- Text input-->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="kontaktperson">Kontaktperson </label>  
-  <div class="col-md-4">
-  <input id="kontaktperson" name="kontaktperson" type="text" placeholder="" class="form-control input-md" required="">
-    
-  </div>
-</div>
-
-<!-- Appended Input-->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="e-post">E-post</label>
-  <div class="col-md-4">
-    <div class="input-group">
-      <input id="e-post" name="e-post" class="form-control" placeholder="" type="text" required="">
-      <span class="input-group-addon"></span>
+  
+   <!------ ANNSETTELSESFORM---- -->
+  <div class="col-lg-6">
+    <div class="w-100">
+    <div class="form-group" style="margin-top: 30px;">
+      <label class="control-label" for="textinput">Ansettelsesform</label>
+      <select id="selectbransje" name="selectbransje" class="form-control">
+        <option value="1">Velg</option>
+        <option value="2">Fulltid</option>
+        <option value="3">Deltid</option>
+        <option value="4">Vikariat</option>
+        <option value="5">Intern</option>
+        <option value="6">Frivillig</option>
+        <option value="7">Annet</option>
+      </select>
     </div>
-    
   </div>
 </div>
-<!-- Appended Input-->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="telefon">Telefon</label>
-  <div class="col-md-4">
-    <div class="input-group">
-      <input id="telefon" name="telefon" class="form-control" placeholder="" type="text" required="">
-      <span class="input-group-addon"></span>
+
+ <!---------------------------------------- ROW 2 ------------------------------------------>
+
+ <!-- --- ARBEIDSTED ----  -->
+ <div class="col-lg-6">
+<div class="form-group" style="margin-top: 30px;">
+  <label class="control-label" for="textinput">Arbeidsted</label>  
+  <input id="textinput" name="textinput" type="text" placeholder="" class="form-control input-lg" required="">
+  </div>
+</div>
+
+
+ <!-- --- SEKTOR ----  -->
+ <div class="col-lg-6">
+<div class="form-group" style="margin-top: 30px;">
+  <label class="control-label" for="textinput">Velg sektor</label>
+      <select id="sektor" name="sektor" class="form-control">
+        <option value="1">Velg</option>
+        <option value="2">Privat</option>
+        <option value="3">Offentlig</option>
+      </select>
     </div>
-    
+  </div>
+
+
+
+
+ <!---------------------------------------- ROW 3  -------------------------------------->
+
+   <!------ STILLINGSTITTEL ---- -->
+  <div class="col-lg-6">
+    <div class="form-group" style="margin-top: 30px;">
+      <label class="control-label" for="textinput">Arbeidsgiver</label>  
+      <input id="textinput" name="textinput" type="text" placeholder="" class="form-control input-lg" required="">
+    </div>
+  </div>
+  
+   <!------ ANNSETTELSESFORM---- -->
+  <div class="col-lg-6">
+    <div class="form-group" style="margin-top: 30px;">
+      <label class="control-label" for="textinput">Bransje</label>
+      <select id="selectbransje" name="selectbransje" class="form-control">
+      <option value="1">Velg</option>
+              <option value="2">Helse og omsorg</option>
+              <option value="3">Varehandel</option>
+              <option value="4">Industri</option>
+              <option value="5">Bygg og anlegg</option>
+              <option value="6">Undervisning</option>
+              <option value="7">Offentlig administrasjon</option>
+              <option value="8">Faglige tjenester</option>
+              <option value="9">IT og medier</option>
+              <option value="10">Olje og gass</option>
+              <option value="">Finans og forsikring</option>
+      </select>
+    </div>
   </div>
 </div>
+ 
+ <!---------------------------------------- ROW 4  -------------------------------------->
+
+ <!-- --- FRIST FOR SØKNAD ----  -->
+ <div class="row">
+  <div class="col-lg-6"> 
+  <div class="form-group" style="margin-top: 30px;">
+  <label class=" control-label" for="textinput">Frist for søknad</label>  
+  <input id="dato" name="dato" type="text" placeholder="01.01.24" class="form-control input-lg" required="">
+</div>
+</div>
+
+   <!-- --- ANT STILLINGER ----  -->
+<div class="col-lg-6">
+<div class="form-group" style="margin-top: 30px;">
+  <label class="control-label" for="antstillinger">Antall stillinger</label>  
+  <input id="textinput" name="textinput" type="text" placeholder="" class="form-control input-lg" required="">
+</div>
+</div>
+</div>
+ <!-- ---------------------------------- STILLINGSBESKRIVELSE ------------------------------>
+
+ <div class="form-group" style="margin-top: 30px;">
+  <label class="col-lg-10 control-label" for="editor" >Stillingsbeskrivelse</label>
+  <div class="col-lg-12" >                     
+    <textarea class="form-control" style="height: 400px;"  id="editor" name="editor"></textarea>
+  </div>
+</div>
+<hr style="padding: 30px;">
+
+
+<!-- ------------------------ HVORDAN SKAL STILLINGEN SØKES? --------------------------- -->
+
+ <!-- --- LENKE TIL SØKNAD ----  -->
+ <h6 style=" color: #9E182F; margin: 15px;"> Hvordan skal stillingen søkes?</h6>
+
+<div class="form-group" style="margin-top: 10px;">
+    <label class="col-md-5 control-label" for="kontaktperson">
+      <p style="color: #4e0000; font-size: 14px;">Via bedriftens eksterne søkeportal</p>
+    </label>
+    <div class="col-lg-12 input-group">
+      <div class=" input-group-prepend">
+        <span class="input-group-text">
+        <i class="fa-sharp fa-solid fa-link"></i>
+        </span>
+      </div>
+      <input id="kontaktperson" name="kontaktperson" type="text" placeholder="" class="form-control input-md" required="">
+    </div>
+  </div>
+
+ <!-- --- E-POST ----  -->
+ <div class="form-group" style="margin-top: 10px;">
+    <label class="col-md-5 control-label" for="kontaktperson">
+      <p style="color: #4e0000; font-size: 14px;">Via e-post</p>
+    </label>
+    <div class="col-lg-5 input-group">
+      <div class="input-group-prepend">
+        <span class="input-group-text">
+        <i class="fa-regular fa-envelope"></i>
+        </span>
+      </div>
+      <input id="kontaktperson" name="kontaktperson" type="text" placeholder="" class="form-control input-md" required="">
+    </div>
+  </div>
+
+<hr style="padding: 30px;">
+  <!-- --- KONTAKT ----  -->
+
+
+  <h6 style="color: #9E182F; margin: 15px;">Kontaktinformasjon</h6>
+
+  <div class="form-group" style="margin-top: 10px;">
+    <label class="col-md-5 control-label" for="kontaktperson">
+      <p style="color: #4e0000; font-size: 14px;">Søker kan kontakte denne personen</p>
+    </label>
+    <div class="col-lg-5 input-group">
+      <div class="input-group-prepend">
+        <span class="input-group-text">
+          <i class="fa-solid fa-user-group"></i>
+        </span>
+      </div>
+      <input id="kontaktperson" name="kontaktperson" type="text" placeholder="" class="form-control input-md" required="">
+    </div>
+  </div>
+
+  <div class="form-group">
+    <label class="col-md-5 control-label" for="telefon">
+      <p style="color: #4e0000; font-size: 14px;">Telefon kontaktperson</p>
+    </label>
+    <div class="col-lg-5 input-group">
+      <div class="input-group-prepend">
+        <span class="input-group-text">
+          <i class="fa-solid fa-phone"></i>
+        </span>
+      </div>
+      <input id="telefon" name="telefon" type="tel" placeholder="" class="form-control input-md" required="">
+    </div>
+  </div>
+
+</div>
+
+ <!-- --- BUTTONS FORHÅNDSVIS + PUBLISER ----  -->
+
 <div class="form-group row">
-                        <div class="col-md-12 text-right">
-                            <button id="button1id" name="button1id" class="btn btn-white">Forhåndsvis</button>
-                            <button id="button2id" name="button2id" class="btn btn-danger">publiser</button>
-                        </div>
-                    </div>
+    <div class="col-lg-7">
+        <p style="padding-top: 30px; color:#7D7DAA; font-size: 14px;">** Annonsen må kvalitetssikres av karrieresenteret før den blir synlig for søkere</p>
+        <button id="button1" type="submit" name="button1" class="btn"style="background-color:#9E182F; color:#fff;"><i class="fa-solid fa-arrow-up-from-bracket"></i> Publiser</button>
+    </div>
+    <div class="col-lg-12 text-right">
+        <button id="button2" name="button2" class="btn" style="background-color:#7D7DAA; color:#fff;"><i class="fa-regular fa-eye"></i> Forhåndsvis</button>
+        <button id="button3" name="button3" class="btn" style="background-color:#7D7DAA; color:#fff;"><i class="fa-regular fa-floppy-disk"></i> Lagre til senere</button>
+    </div>
+</div>
+
                 </fieldset>
             </div>
         </div>
--- page-templates/job-posting-page.php
+++ page-templates/job-posting-page.php
@@ -4,27 +4,20 @@ Template Name: Job Posting Page
 */
 get_header();
 ?>
+<div class="main-content">
+<div class="row">
 
-
-<div id="wrapper" class="d-flex">
-    <!-- Sidebar -->
-    <div class="bg-red p-4" id="sidebar-wrapper">
+    <div class="col-md-8">
+        
     <?php get_template_part('includes/controlpanel-sidebar'); ?>
     </div>
-
-  <div id="page-content-wrapper">
-    <div class="container">
-      <div class="text-left">
-        <h2 class="mt-4" style="color: black;">Her oppretter du din annonse Marianne!</h2>
-        
-      </div>
-
-    <!-- Cards Section -->
-        <div class="card-container mt-4">
+    
+    <div class="container mt-4"> 
         <?php get_template_part('includes/job-posting-form'); ?>         
-          </div>
-          
-        </div>
     </div>
-</div>
-<?php get_footer(); ?>
+ 
+    </div>
+  <?php get_footer(); ?>
+ <script>
+    
+ </script>
